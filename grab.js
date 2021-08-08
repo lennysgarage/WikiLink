14e4f2f07b6e1023b35c3b56afb9e86c46e0f258
lennysgarage <lennysgarage1@gmail.com>
2021-08-08 17:50:04 -0400

    API link easier to maintain and allowed for longer extracts

--- grab.js
+++ grab.js
@@ -1,5 +1,22 @@
 const fetch = require("node-fetch");
 
+let url = "https://en.wikipedia.org/w/api.php";
+
+const params = {
+    action: "query",
+    format: "json",
+    generator: "random",
+    prop: "extracts",
+    exlimit: "1",
+    exintro: "1",
+    explaintext: "1",
+    grnnamespace: "0",
+    grnlimit: "1"
+};
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+
 async function graby() {
     let contents = "";
     while(contents.length < 3) {
@@ -11,22 +28,26 @@ async function graby() {
 
 function grabWikiContents() {
 
-    return fetch("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&prop=extracts&exlimit=max&explaintext&exintro")
+    return fetch(url)
         .then(response => response.json())
         .then(commits => {
             let key;
             for(key in commits.query.pages) {
                 if(commits.query.pages.hasOwnProperty(key)) {
-                    return JSON.stringify(commits.query.pages[key].extract).substring(1, 201).replaceAll(" ", "-");
+                    let content = JSON.stringify(commits.query.pages[key].extract);
+                    return content.substring(1,content.length-1).replaceAll(" ", "-");
+                    // We substring to remove quotation marks at beginning and end
                 }
             }
-        });
+        })
+        .catch(console.error);
 }
 
 
 module.exports.grabWikiContents = graby;
+//Basic testing code 
 // async function main() {
-//     graby().then(content => console.log(content));
+//      graby().then(content => console.log(content));
 // }
 
 // main().catch(console.error);
