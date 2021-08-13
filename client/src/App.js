433b4e2d7093cc6aae664f27c2b63c3d331fdd5d
lennysgarage <lennysgarage1@gmail.com>
2021-08-13 17:02:22 -0400

    First working react version

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      original_url: '',
+      wikified_url: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ original_url: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A url was submitted ' + this.state.original_url);
+    event.preventDefault();
+    axios.post('http://localhost:9000/wikiurl/', {
+      url: this.state.original_url
+    })
+    .then(response => {
+      console.log(response.data);
+      this.setState({ wikified_url: response.data.wikified_url})
+    });
+  }
+
+  render() {
+    return (
+      <div className='WikiLink'>
+        <h1> WikiLink</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            URL:
+            <input type="text" placeholder="https://github.com/lennysgarage" value={this.state.url} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Wikify"/>
+        </form>
+        <p>{this.state.wikified_url}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
