42680a26e4f293fe2bcdbf7e8abfdf5efd713ffe
lennysgarage <lennysgarage1@gmail.com>
2021-08-23 21:08:13 -0400

    feat: add dark mode toggle

---
+++ client/src/components/Toggle.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import '../styles/toggle.css';
+import { setTheme } from '../utils/themes';
+
+function Toggle() {
+    const [togClass, setTogClass] = useState('light');
+    let theme = localStorage.getItem('theme');
+
+    const handleOnClick = () => {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+            setTheme('theme-light');
+            setTogClass('light');
+        } else {
+            setTheme('theme-dark');
+            setTogClass('dark');
+        }
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+            setTogClass('dark');
+        } else if (localStorage.getItem('theme') === 'theme-light') {
+            setTogClass('light');
+        }
+    }, [theme]);
+
+
+    return (
+        <div className="container--toggle">
+            {
+                togClass === "light" ?
+                <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={handleOnClick} checked />
+                :
+                <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={handleOnClick} />
+            }            
+            <label htmlFor="toggle" className="toggle--label">
+                <span className="toggle--label-background"></span>
+            </label>
+        </div>
+    )
+}
+
+export default Toggle;
