fd05605e14ee67de998dd4fc28a6a4cb1857a8f7
lennysgarage <lennysgarage1@gmail.com>
2021-08-23 21:07:39 -0400

    refactor: completely changing main app layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,23 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import './styles/App.css';
+import { useEffect } from 'react';
+import { keepTheme } from './utils/themes';
+import Header from './components/Header';
+import Toggle from './components/Toggle';
+import Wikify from './components/wikifyLink'; 
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      original_url: '',
-      wikified_url: ''
-    };
+function App() {
+  useEffect(() => {
+    keepTheme();
+  });
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
-  handleChange(event) {
-    this.setState({ original_url: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert('A url was submitted ' + this.state.original_url);
-    event.preventDefault();
-    axios.post('http://localhost:9000/wikiurl/', {
-      url: this.state.original_url
-    })
-    .then(response => {
-      console.log(response.data);
-      this.setState({ wikified_url: response.data.wikified_url})
-    });
-  }
-
-  render() {
-    return (
-      <div className='WikiLink'>
-        <h1> WikiLink</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            URL:
-            <input type="text" placeholder="https://github.com/lennysgarage" value={this.state.url} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Wikify"/>
-        </form>
-        <p>{this.state.wikified_url}</p>
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <Header />
+      <Toggle />
+      <Wikify />
+    </div>
+  )
 }
 
 export default App;
