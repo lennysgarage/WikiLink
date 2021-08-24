328fee255fd46e0f07b84b25ebc13bf94c98d3dc
lennysgarage <lennysgarage1@gmail.com>
2021-08-24 15:53:31 -0400

    feat: add helmet for http header protection

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var helmet = require('helmet');
 
 var connection = require('./utils/mongo_connection.js');
 
@@ -18,6 +19,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(helmet());
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
