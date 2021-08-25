8c9876c0eb728b3458092d2d4f9dc698cbeac7fd
lennysgarage <lennysgarage1@gmail.com>
2021-08-25 17:19:05 -0400

    feat: add particles & organize styling

---
+++ client/src/components/App.js
@@ -0,0 +1,26 @@
+import '../styles/App.css';
+import { useEffect } from 'react';
+import { keepTheme } from '../utils/themes';
+import Header from './Header';
+import Toggle from './Toggle';
+import Wikify from './wikifyLink'; 
+import Particles from 'react-tsparticles';
+import particleOptions from '../utils/particles.json';
+
+function App() {
+  useEffect(() => {
+    keepTheme();
+  });
+
+
+  return (
+    <div className="App">
+      <Particles options={particleOptions} />
+      <Header />
+      <Toggle />
+      <Wikify />
+    </div>
+  )
+}
+
+export default App;
