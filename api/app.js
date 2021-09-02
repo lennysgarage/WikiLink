0829cc272083bcdb3ff5b131685b7fa27128107a
lennysgarage <lennysgarage1@gmail.com>
2021-09-01 23:05:59 -0400

    feat:only grab wiki article if an article is needed & include pageid,title

--- api/app.js
+++ api/app.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', wikifyRouter);
 app.use('/:wikified?', wikifiedRouter);
 
