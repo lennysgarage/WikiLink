5b8286250b44c06a9b905014bc252f3099e4695e
lennysgarage <lennysgarage1@gmail.com>
2022-08-31 21:47:09 -0400

    fix: change wikilink domain
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- client/src/components/wikifyLink.js
+++ client/src/components/wikifyLink.js
@@ -5,84 +5,84 @@ import Button from 'react-bootstrap/Button';
 import WikiCard from './wikiCard';
 
 class Wikify extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        original_url: '',
-        wikified_url: '',
-        title: '',
-        pageid: '',
-        isLoading: false
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.urlToPost = process.env.NODE_ENV === 'development' ? 'http://localhost:9000/' : 'https://api.wikifylink.me/';
-    }
+  constructor() {
+    super();
+    this.state = {
+      original_url: '',
+      wikified_url: '',
+      title: '',
+      pageid: '',
+      isLoading: false
+    };
 
-    /* Update value of url inputted */
-    handleChange(event) {
-      this.setState({ original_url: event.target.value }); 
-    }
-
-    /* Here we are submitting the link to wikify */
-    async handleSubmit(event) {
-      event.preventDefault();
-      if (!validUrl.isWebUri(this.state.original_url)) {
-        alert('Invalid url');
-        return;
-      }
-
-      this.setState({ isLoading: true });
-
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ url: this.state.original_url }),
-      };
-      const response = await fetch(this.urlToPost, requestOptions);
-      const data = await response.json();
-      this.setState({
-        wikified_url: data.wikified_url,
-        pageid: data.pageid,
-        title: data.title,
-        isLoading: false
-      });
-    }
-  
-    render() {
-      return (
-        <div className='WikifyInput'>
-          <Form>
-            <Form.Group className="mb-3" controlId="originalLink">
-              <Form.Control 
-                required
-                type="url" 
-                placeholder="e.g. https://github.com/lennysgarage/WikiLink" 
-                onChange={this.handleChange} 
-              />
-            </Form.Group>
-            <div className="d-grid gap-2">
-              <Button 
-                variant="primary"
-                type="submit"
-                size="lg"
-                disabled={this.state.isLoading}
-                onClick={!this.state.isLoading ? this.handleSubmit : null}
-              >
-                {this.state.isLoading ? 'Wikifying...' : 'Click to Wikify'}
-              </Button>
-            </div>
-          </Form>
-          <div className="d-grid gap-1">
-            <br/>
-            {this.state.wikified_url && <Button variant="secondary" size="lg" onClick={() => {navigator.clipboard.writeText(this.urlToPost+this.state.wikified_url)}}
-  >Copy Wikified Link!</Button>} 
-          </div>
-          {this.state.wikified_url && <WikiCard title={this.state.title} content={this.urlToPost+this.state.wikified_url} link={`https://en.wikipedia.org/w/index.php?curid=${this.state.pageid}`} />} 
-        </div>
-      )
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.urlToPost = process.env.NODE_ENV === 'development' ? 'http://localhost:9000/' : 'https://wikilink-backend.herokuapp.com/';
   }
+
+  /* Update value of url inputted */
+  handleChange(event) {
+    this.setState({ original_url: event.target.value });
+  }
+
+  /* Here we are submitting the link to wikify */
+  async handleSubmit(event) {
+    event.preventDefault();
+    if (!validUrl.isWebUri(this.state.original_url)) {
+      alert('Invalid url');
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: this.state.original_url }),
+    };
+    const response = await fetch(this.urlToPost, requestOptions);
+    const data = await response.json();
+    this.setState({
+      wikified_url: data.wikified_url,
+      pageid: data.pageid,
+      title: data.title,
+      isLoading: false
+    });
+  }
+
+  render() {
+    return (
+      <div className='WikifyInput'>
+        <Form>
+          <Form.Group className="mb-3" controlId="originalLink">
+            <Form.Control
+              required
+              type="url"
+              placeholder="e.g. https://github.com/lennysgarage/WikiLink"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <div className="d-grid gap-2">
+            <Button
+              variant="primary"
+              type="submit"
+              size="lg"
+              disabled={this.state.isLoading}
+              onClick={!this.state.isLoading ? this.handleSubmit : null}
+            >
+              {this.state.isLoading ? 'Wikifying...' : 'Click to Wikify'}
+            </Button>
+          </div>
+        </Form>
+        <div className="d-grid gap-1">
+          <br />
+          {this.state.wikified_url && <Button variant="secondary" size="lg" onClick={() => { navigator.clipboard.writeText(this.urlToPost + this.state.wikified_url) }}
+          >Copy Wikified Link!</Button>}
+        </div>
+        {this.state.wikified_url && <WikiCard title={this.state.title} content={this.urlToPost + this.state.wikified_url} link={`https://en.wikipedia.org/w/index.php?curid=${this.state.pageid}`} />}
+      </div>
+    )
+  }
+}
 
 export default Wikify;
