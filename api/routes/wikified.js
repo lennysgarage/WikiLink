665ce88144d09563ef22f322192ac992b8d38ff6
lennysgarage <lennysgarage1@gmail.com>
2024-04-07 13:43:48 -0400

    Mongodb findOne no longer accepts a callback
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- api/routes/wikified.js
+++ api/routes/wikified.js
@@ -3,20 +3,19 @@ const router = express.Router({ mergeParams: true });
 
 const URL = require('../model/url.js');
 
-
 /* convert shorturl to original URL */ 
-router.get('/', (req, res) => {
-    URL.findOne({ wikified_url: req.params.wikified }, (error, urlFound) => {
-      if (error) {
-        console.log(error);
-      } else {
+router.get('/', async (req, res) => {
+    try {
+        const urlFound = await URL.findOne({ wikified_url: req.params.wikified });
         if (!urlFound) {
-          return res.status(404).json("URL not found");
+            return res.status(404).json("URL not found");
         } else {
-          return res.redirect(urlFound.original_url);
+            return res.redirect(urlFound.original_url);
         }
-      }
-    });
-  });
-  
+    } catch (error) {
+        console.error(error);
+        res.status(500).json("An error occurred while fetching the URL");
+    }
+});
+
 module.exports = router;
--- api/routes/wikify.js
+++ api/routes/wikify.js
@@ -2,55 +2,50 @@ const express = require('express');
 const validUrl = require('valid-url');
 const router = express.Router();
 
-
 const URL = require('../model/url.js');
 const { WikiContents } = require('../utils/grab.js');
-
 
 /* Wikifying a new URL */
 router.post('/', async (req, res) => {
     const url = req.body.url;
-
 
     if (!validUrl.isWebUri(url)) {
       res.json({
         error: 'invalid url'
       });
     } else {
-      URL.findOne({ original_url: url }, async (error, urlFound) => {
-        if (error) {
-          console.error(error);
-        } else {
-            let content;
-            try {
-              content = await WikiContents();
-            } catch (err) {
-              console.error('Error on requesting content', err);
-            }
-
-            let wikified_contents = content.content;
-            let pageid = content.pageid;
-            let title = content.title;
-            // Want to find the wiki url
-            try {
-              // Don't need to save title & pageid in db
-              newURL = new URL({
-                original_url: url,
-                wikified_url: wikified_contents,
-              });
-              newURL.save(); 
-              res.json({
-                original_url: url,
-                wikified_url: wikified_contents,
-                title: title,
-                pageid:  pageid
-              });
-          } catch (err) {
-            console.error('Error on saving to database', err);
-          }
+      try {
+        const urlFound = await URL.findOne({ original_url: url });
+        let content;
+        try {
+          content = await WikiContents();
+        } catch (err) {
+          console.error('Error on requesting content', err);
         }
-      });
+
+        let wikified_contents = content.content;
+        let pageid = content.pageid;
+        let title = content.title;
+
+        try {
+          const newURL = new URL({
+            original_url: url,
+            wikified_url: wikified_contents,
+          });
+          await newURL.save(); 
+          res.json({
+            original_url: url,
+            wikified_url: wikified_contents,
+            title: title,
+            pageid:  pageid
+          });
+        } catch (err) {
+          console.error('Error on saving to database', err);
+        }
+      } catch (error) {
+        console.error(error);
+      }
     }
-  });
+});
 
 module.exports = router;
