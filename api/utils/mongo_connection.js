1de32289a7eaa054c5c08c3ba7766278b2cc3b2e
Jonathan Marcantonio <lennysgarage1@gmail.com>
2024-04-07 13:29:39 -0400

    Update backend packages (#4)
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- api/utils/mongo_connection.js
+++ api/utils/mongo_connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 
 /* Connect to MongoDB */
 mongoose.connect(process.env['MONGO_URI'], {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000
 });
   
